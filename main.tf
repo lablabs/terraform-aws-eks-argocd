ffa668ef6b3a783a46525f10ebe166a6635a120a
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-05-26 22:25:45 +0200

    refactor: simplify addon IRSA key names in main.tf

--- main.tf
+++ main.tf
@@ -17,42 +17,42 @@ locals {
   }
 
   addon_irsa = {
-    "${local.addon.name}-application-controller" = {
+    application-controller = {
       service_account_create   = var.application_controller_service_account_create
       service_account_name     = var.application_controller_service_account_name
       irsa_role_create         = var.application_controller_irsa_role_create
       irsa_role_name           = "application"
       irsa_additional_policies = var.application_controller_irsa_additional_policies
     }
-    "${local.addon.name}-applicationset-controller" = {
+    applicationset-controller = {
       service_account_create   = var.applicationset_controller_service_account_create
       service_account_name     = var.applicationset_controller_service_account_name
       irsa_role_create         = var.applicationset_controller_irsa_role_create
       irsa_role_name           = "applicationset"
       irsa_additional_policies = var.applicationset_controller_irsa_additional_policies
     }
-    "${local.addon.name}-notifications-controller" = {
+    notifications-controller = {
       service_account_create   = var.notifications_controller_service_account_create
       service_account_name     = var.notifications_controller_service_account_name
       irsa_role_create         = var.notifications_controller_irsa_role_create
       irsa_role_name           = "notification"
       irsa_additional_policies = var.notifications_controller_irsa_additional_policies
     }
-    "${local.addon.name}-server" = {
+    server = {
       service_account_create   = var.server_service_account_create
       service_account_name     = var.server_service_account_name
       irsa_role_create         = var.server_irsa_role_create
       irsa_role_name           = "server"
       irsa_additional_policies = var.server_irsa_additional_policies
     }
-    "${local.addon.name}-dex-server" = {
+    dex-server = {
       service_account_create   = var.dex_server_service_account_create
       service_account_name     = var.dex_server_service_account_name
       irsa_role_create         = var.dex_server_irsa_role_create
       irsa_role_name           = "dex-server"
       irsa_additional_policies = var.dex_server_irsa_additional_policies
     }
-    "${local.addon.name}-repo-server" = {
+    repo-server = {
       service_account_create   = var.repo_server_service_account_create
       service_account_name     = var.repo_server_service_account_name
       irsa_role_create         = var.repo_server_irsa_role_create
@@ -64,55 +64,55 @@ locals {
   addon_values = yamlencode({
     controller = {
       serviceAccount = {
-        create = local.addon_irsa["${local.addon.name}-application-controller"].service_account_create
-        name   = local.addon_irsa["${local.addon.name}-application-controller"].service_account_name
-        annotations = module.addon-irsa["${local.addon.name}-application-controller"].irsa_role_enabled ? {
-          "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-application-controller"].iam_role_attributes.arn
+        create = local.addon_irsa["application-controller"].service_account_create
+        name   = local.addon_irsa["application-controller"].service_account_name
+        annotations = module.addon-irsa["application-controller"].irsa_role_enabled ? {
+          "eks.amazonaws.com/role-arn" = module.addon-irsa["application-controller"].iam_role_attributes.arn
         } : tomap({})
       }
     }
     applicationSet = {
       serviceAccount = {
-        create = local.addon_irsa["${local.addon.name}-applicationset-controller"].service_account_create
-        name   = local.addon_irsa["${local.addon.name}-applicationset-controller"].service_account_name
-        annotations = module.addon-irsa["${local.addon.name}-applicationset-controller"].irsa_role_enabled ? {
-          "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-applicationset-controller"].iam_role_attributes.arn
+        create = local.addon_irsa["applicationset-controller"].service_account_create
+        name   = local.addon_irsa["applicationset-controller"].service_account_name
+        annotations = module.addon-irsa["applicationset-controller"].irsa_role_enabled ? {
+          "eks.amazonaws.com/role-arn" = module.addon-irsa["applicationset-controller"].iam_role_attributes.arn
         } : tomap({})
       }
     }
     notifications = {
       serviceAccount = {
-        create = local.addon_irsa["${local.addon.name}-notifications-controller"].service_account_create
-        name   = local.addon_irsa["${local.addon.name}-notifications-controller"].service_account_name
-        annotations = module.addon-irsa["${local.addon.name}-notifications-controller"].irsa_role_enabled ? {
-          "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-notifications-controller"].iam_role_attributes.arn
+        create = local.addon_irsa["notifications-controller"].service_account_create
+        name   = local.addon_irsa["notifications-controller"].service_account_name
+        annotations = module.addon-irsa["notifications-controller"].irsa_role_enabled ? {
+          "eks.amazonaws.com/role-arn" = module.addon-irsa["notifications-controller"].iam_role_attributes.arn
         } : tomap({})
       }
     }
     server = {
       serviceAccount = {
-        create = local.addon_irsa["${local.addon.name}-server"].service_account_create
-        name   = local.addon_irsa["${local.addon.name}-server"].service_account_name
-        annotations = module.addon-irsa["${local.addon.name}-server"].irsa_role_enabled ? {
-          "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-server"].iam_role_attributes.arn
+        create = local.addon_irsa["server"].service_account_create
+        name   = local.addon_irsa["server"].service_account_name
+        annotations = module.addon-irsa["server"].irsa_role_enabled ? {
+          "eks.amazonaws.com/role-arn" = module.addon-irsa["server"].iam_role_attributes.arn
         } : tomap({})
       }
     }
     dex = {
       serviceAccount = {
-        create = local.addon_irsa["${local.addon.name}-dex-server"].service_account_create
-        name   = local.addon_irsa["${local.addon.name}-dex-server"].service_account_name
-        annotations = module.addon-irsa["${local.addon.name}-dex-server"].irsa_role_enabled ? {
-          "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-dex-server"].iam_role_attributes.arn
+        create = local.addon_irsa["dex-server"].service_account_create
+        name   = local.addon_irsa["dex-server"].service_account_name
+        annotations = module.addon-irsa["dex-server"].irsa_role_enabled ? {
+          "eks.amazonaws.com/role-arn" = module.addon-irsa["dex-server"].iam_role_attributes.arn
         } : tomap({})
       }
     }
     repoServer = {
       serviceAccount = {
-        create = local.addon_irsa["${local.addon.name}-repo-server"].service_account_create
-        name   = local.addon_irsa["${local.addon.name}-repo-server"].service_account_name
-        annotations = module.addon-irsa["${local.addon.name}-repo-server"].irsa_role_enabled ? {
-          "eks.amazonaws.com/role-arn" = module.addon-irsa["${local.addon.name}-repo-server"].iam_role_attributes.arn
+        create = local.addon_irsa["repo-server"].service_account_create
+        name   = local.addon_irsa["repo-server"].service_account_name
+        annotations = module.addon-irsa["repo-server"].irsa_role_enabled ? {
+          "eks.amazonaws.com/role-arn" = module.addon-irsa["repo-server"].iam_role_attributes.arn
         } : tomap({})
       }
     }
